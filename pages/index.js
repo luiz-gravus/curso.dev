c552517454a4d305d2b16ee68870a63d1cee96ee
luiz-gravus <luiz.rodrigues@gravus.com.br>
2025-05-06 20:02:16 +0000

    adicionando o link check e fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Amorzinho, eu te amo. Se vc tambem me ama da uma risadinha!</h1>
+function Home() {
+  return <h1>Amorzinho, eu te amo. Se vc tambem me ama da uma risadinha!</h1>;
+}
+
+function (){
+  console.log('olá mundo')
 }
 
 export default Home;
